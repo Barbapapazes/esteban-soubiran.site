e8556da61e480437813ecac70ab797d0fe562481
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-01-05 01:10:10 +0100

    feat: first draft of the home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,38 @@
 <template>
-  <div>hello</div>
+  <div>
+    <div class="h-screen p-20 flex flex-col justify-between">
+      <header class="flex justify-between">
+        <h1 class="text-3xl text-light-grey font-text">Estéban SOUBIRAN</h1>
+        <img src="~/assets/logo/github-light.svg" alt="logo de GitHub" />
+      </header>
+      <section class="h-full flex flex-col justify-center">
+        <div class="flex flex-row justify-between">
+          <div>
+            <h2 class="text-8xl text-white font-text">&#60; code &#62;</h2>
+            <p class="text-light-grey text-3xl font-text">
+              Ingénieur en sécurité informatique en pleine apprentissage
+            </p>
+            <p class="text-light-grey text-3xl mt-5 font-text">
+              Développeur web dans son temps libre
+            </p>
+          </div>
+          <img
+            src="~/assets/img/pp-esteban.svg"
+            alt="photo de profil de Estéban"
+            class="shadow-small rounded-full shadow-medium"
+          />
+        </div>
+        <div
+          class="flex flex-row justify-between w-64 mt-10 p-3 shadow-inner-small rounded-5"
+        >
+          <img src="~/assets/logo/facebook.svg" alt="logo de Facebook" />
+          <img src="~/assets/logo/linkedin.svg" alt="logo de Linkedin" />
+          <img src="~/assets/logo/github-dark.svg" alt="logo de Github" />
+        </div>
+      </section>
+      <hr class="bg-white h-1 rounded-full" />
+    </div>
+  </div>
 </template>
 
 <script>
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'deep-blue': '#344163',
+        'light-grey': '#D7D7D7',
+      },
+      fontSize: {
+        '8xl': '6rem',
+      },
+      boxShadow: {
+        'inner-small':
+          'inset -5px -5px 8px 0 rgb(62, 78, 119), inset 5px 5px 8px 0 rgb(42, 52, 79)',
+        medium:
+          '-9px -9px 15px 0 rgb(62, 78, 119), 9px 9px 15px 0 rgb(42, 52, 79)',
+      },
+      borderRadius: {
+        5: '5px',
+      },
+      fontFamily: {
+        text: ['CascadiaCode', 'sans-serif'],
+      },
+    },
+  },
+}
