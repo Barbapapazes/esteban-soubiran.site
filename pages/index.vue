96eb04fd53cab03645fd01b095660e7d278aaf0b
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-01-05 01:55:45 +0100

    feat: resize image

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,12 @@
     <div class="h-screen p-20 flex flex-col justify-between">
       <header class="flex justify-between">
         <h1 class="text-3xl text-light-grey font-text">Estéban SOUBIRAN</h1>
-        <img src="~/assets/logo/github-light.svg" alt="logo de GitHub" />
+        <img
+          src="~/assets/logo/github-light.svg"
+          alt="logo de GitHub"
+          width="48"
+          height="48"
+        />
       </header>
       <section class="h-full flex flex-col justify-center">
         <div class="flex flex-row justify-between">
@@ -16,18 +21,37 @@
               Développeur web dans son temps libre
             </p>
           </div>
-          <img
-            src="~/assets/img/pp-esteban.svg"
-            alt="photo de profil de Estéban"
-            class="shadow-small rounded-full shadow-medium"
-          />
+          <div height="250" class="flex flex-col justify-center">
+            <img
+              src="~/assets/img/pp-esteban.webp"
+              alt="photo de profil de Estéban"
+              class="shadow-small rounded-full shadow-medium"
+              width="250"
+              height="250"
+            />
+          </div>
         </div>
         <div
           class="flex flex-row justify-between w-64 mt-10 p-3 shadow-inner-small rounded-5"
         >
-          <img src="~/assets/logo/facebook.svg" alt="logo de Facebook" />
-          <img src="~/assets/logo/linkedin.svg" alt="logo de Linkedin" />
-          <img src="~/assets/logo/github-dark.svg" alt="logo de Github" />
+          <img
+            src="~/assets/logo/facebook.svg"
+            alt="logo de Facebook"
+            width="48"
+            height="48"
+          />
+          <img
+            src="~/assets/logo/linkedin.svg"
+            alt="logo de Linkedin"
+            width="48"
+            height="48"
+          />
+          <img
+            src="~/assets/logo/github-dark.svg"
+            alt="logo de Github"
+            width="48"
+            height="48"
+          />
         </div>
       </section>
       <hr class="bg-white h-1 rounded-full" />
