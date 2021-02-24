7340ccff2c75cb987cad77a6d9ef773119a2c4d4
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-02-24 14:01:37 +0100

    feat(hero): add animations

--- components/organisms/TheHero.vue
+++ components/organisms/TheHero.vue
@@ -5,22 +5,27 @@
         <transition name="slideup" appear>
           <Title>code</Title>
         </transition>
-        <div>
-          <Subtitle class="mb-3"
-            >Élève-ingénieur en sécurité informatique</Subtitle
-          >
-          <Subtitle class="mb-3">
-            Développeur web dans son temps libre</Subtitle
-          >
-          <Subtitle> Passionné par le monde associatif</Subtitle>
-        </div>
-        <img
-          src="~/assets/img/pp-esteban.webp"
-          alt="photo de profil de Estéban"
-          class="shadow-small rounded-full shadow-medium w-2/4"
-          width="250"
-          height="250"
-        />
+        <transition name="slideup" appear>
+          <div style="transition-delay: 0.1s">
+            <Subtitle class="mb-3"
+              >Élève-ingénieur en sécurité informatique</Subtitle
+            >
+            <Subtitle class="mb-3">
+              Développeur web dans son temps libre</Subtitle
+            >
+            <Subtitle> Passionné par le monde associatif</Subtitle>
+          </div>
+        </transition>
+        <transition name="fade-long" appear>
+          <img
+            style="transition-delay: 0.4s"
+            src="~/assets/img/pp-esteban.webp"
+            alt="photo de profil de Estéban"
+            class="shadow-small rounded-full shadow-medium w-2/4"
+            width="250"
+            height="250"
+          />
+        </transition>
       </div>
     </div>
     <hr class="bg-white h-small rounded-full" />
