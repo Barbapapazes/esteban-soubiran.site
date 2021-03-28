c6ebaad9fa93600ddd4825a20b58b60cb0e5421d
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-03-28 17:33:03 +0200

    build(tailwindcss): add jit

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,6 +56,10 @@ export default {
     '@nuxtjs/tailwindcss',
   ],
 
+  tailwindcss: {
+    jit: true,
+  },
+
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/pwa
