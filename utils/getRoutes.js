51d654b003fcac87a77edff58bebcfd8dce30718
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-01-04 18:33:01 +0100

    fix(sitemap): remove template

--- utils/getRoutes.js
+++ utils/getRoutes.js
@@ -2,5 +2,8 @@ export default async () => {
   const { $content } = require('@nuxt/content')
   const files = await $content({ deep: true }).only(['path']).fetch()
 
-  return files.map((file) => (file.path === '/index' ? '/' : file.path))
+  // Remove template file from the list
+  const filteredFiles = files.filter((file) => file.path !== '/template')
+
+  return filteredFiles.map((file) => (file.path === '/index' ? '/' : file.path))
 }
