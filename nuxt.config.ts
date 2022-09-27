52cc35ede148154894cc24a54f42554c5b858575
Barbapapazes <e.soubiran25@gmail.com>
2022-09-27 20:39:37 +0200

    feat: add tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content'],
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
   content: {
     documentDriven: true,
   },
