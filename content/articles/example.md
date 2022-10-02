97261f6073e16b1c9821723f04bf357d4fccc37b
Barbapapazes <e.soubiran25@gmail.com>
2022-10-02 19:26:28 +0200

    feat: add image for article layout

---
+++ content/articles/example.md
@@ -0,0 +1,9 @@
+---
+title: Un article test
+createdAt: 2022-09-07
+layout: article
+---
+
+Lorem ipsum
+
+Another tag P
--- layouts/article.vue
+++ layouts/article.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
 const { page } = useContent()
-const date = new Date(page.value.date)
+
+const image = ref(page.value.image)
+const createdAt = ref(new Date(page.value.createdAt))
 </script>
 
 <template>
@@ -12,11 +14,12 @@ const date = new Date(page.value.date)
       <time class="order-first flex flex-row items-center text-base dark:text-zinc-500 font-semibold">
         <span class="h-4 w-0.5 rounded-full dark:bg-zinc-500"></span>
         <span class="ml-3">
-          {{ date.toLocaleDateString('fr-FR', { year: 'numeric', month: 'long', day: 'numeric' }) }}
+          {{ createdAt.toLocaleDateString('fr-FR', { year: 'numeric', month: 'long', day: 'numeric' }) }}
         </span>
       </time>
     </div>
-    <div class="mx-auto prose">
+    <img v-if="image" :src="image.src" :alt="image.alt" class="mt-6 rounded-2xl">
+    <div class="mt-6 mx-auto prose w-full">
       <slot />
     </div>
   </article>
