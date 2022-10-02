a82060ba7cc5b1b10abe745628b150fa5895f411
Barbapapazes <esteban.soubiran@insa-cvl.fr>
2022-10-02 19:28:31 +0200

    feat: create article layout (#277)
    * feat: create article page
    
    * fix: footer links
    
    * feat: add page for articles
    
    * feat: add image for article layout

---
+++ layouts/article.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+const { page } = useContent()
+
+const image = ref(page.value.image)
+const createdAt = ref(new Date(page.value.createdAt))
+</script>
+
+<template>
+  <article class="max-w-2xl mx-auto flex flex-col">
+    <div class="flex flex-col gap-4">
+      <h1 class="dark:text-zinc-100 text-5xl font-bold tracking-tight">
+        {{ page.title }}
+      </h1>
+      <time class="order-first flex flex-row items-center text-base dark:text-zinc-500 font-semibold">
+        <span class="h-4 w-0.5 rounded-full dark:bg-zinc-500"></span>
+        <span class="ml-3">
+          {{ createdAt.toLocaleDateString('fr-FR', { year: 'numeric', month: 'long', day: 'numeric' }) }}
+        </span>
+      </time>
+    </div>
+    <img v-if="image" :src="image.src" :alt="image.alt" class="mt-6 rounded-2xl">
+    <div class="mt-6 mx-auto prose w-full">
+      <slot />
+    </div>
+  </article>
+</template>
--- windi.config.ts
+++ windi.config.ts
@@ -1,6 +1,39 @@
-// @ts-check - enable TS check for js file
+import typography from 'windicss/plugin/typography'
+import colors from 'windicss/colors'
 import { defineConfig } from 'windicss/helpers'
 
 export default defineConfig({
-  plugins: [require('windicss/plugin/typography')],
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            'max-width': 'none',
+            p: {
+              'margin-top': '1.5rem',
+              'margin-bottom': '1.5rem',
+            },
+          },
+        },
+        DARK: {
+          css: {
+            color: colors.zinc[400],
+            a: {
+              color: colors.sky[500],
+              'font-weight': 600,
+              'text-decoration': 'underline',
+              'text-decoration-color': 'hsla(199, 89%, 48%, 0.3)',
+              'transition-property': 'color,text-decoration',
+              'transition-duration': '0.15s',
+              'transition-timing-function': 'ease-in',
+              '&:hover': {
+                'text-decoration-color': colors.sky[500],
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  plugins: [typography({ dark: true })],
 })
