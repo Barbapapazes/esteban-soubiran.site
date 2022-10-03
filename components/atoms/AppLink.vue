b8785b38beded8a887017d02b21e99d6ed29f4af
Barbapapazes <esteban.soubiran@insa-cvl.fr>
2022-10-03 19:10:51 +0200

    feat: create header (#286)

--- components/atoms/AppLink.vue
+++ components/atoms/AppLink.vue
@@ -1,5 +1,12 @@
+<script lang="ts" setup>
+defineProps<{
+  large?: boolean
+}>()
+</script>
+
 <template>
-  <NuxtLink class="px-3 py-2 text-sm text-zinc-200 font-semibold hover:text-sky-500 transition-colors ease-in">
+  <NuxtLink class="inline-block px-3 py-2 dark:text-zinc-200 font-semibold hover:text-sky-500 transition-colors ease-in"
+    :class="{'text-sm': !large, 'text-base': large}">
     <slot />
   </NuxtLink>
 </template>
