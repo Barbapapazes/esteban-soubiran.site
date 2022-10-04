9483d0e9eb33a4051bd3eede318c3f51ec299999
Barbapapazes <e.soubiran25@gmail.com>
2022-10-04 07:37:20 +0200

    feat: create articles cards

---
+++ components/content/organisms/ArticlesList.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const { data: articles } = await useAsyncData('articles', () => queryContent('/articles/').find())
+</script>
+
+<template>
+  <section>
+    <h2 class="sr-only">Liste des articles</h2>
+    <ArticleCard v-for="article in articles" :article="article" :key="article._path" />
+
+    <pre class="text-white">
+      {{ articles }}
+    </pre>
+  </section>
+</template>
