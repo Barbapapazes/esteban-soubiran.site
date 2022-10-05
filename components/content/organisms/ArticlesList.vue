bb53af5e4a7f96a32b7026ef2e1e3cbc5e056eae
Barbapapazes <e.soubiran25@gmail.com>
2022-10-05 07:17:59 +0200

    fix: padding

--- components/content/organisms/ArticlesList.vue
+++ components/content/organisms/ArticlesList.vue
@@ -3,12 +3,12 @@ const { data: articles } = await useAsyncData('articles', () => queryContent('/a
 </script>
 
 <template>
-  <section>
-    <h2 class="sr-only">Liste des articles</h2>
-    <ArticleCard v-for="article in articles" :article="article" :key="article._path" />
-
-    <pre class="text-white">
-      {{ articles }}
-    </pre>
+  <section class="relative px-4 sm:px-8 lg:px-16">
+    <div class="max-w-screen-xl mx-auto sm:px-8">
+      <div class=" max-w-2xl lg:max-w-5xl mx-auto">
+        <h2 class="sr-only">Liste des articles</h2>
+        <ArticleCard v-for="article in articles" :article="article" :key="article._path" />
+      </div>
+    </div>
   </section>
 </template>
