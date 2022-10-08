5941d94973f7e4ac32baa19c9487ecbd578bd0af
Barbapapazes <e.soubiran25@gmail.com>
2022-10-08 13:59:55 +0200

    fix: enable shiki

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   modules: ['@nuxt/content', 'nuxt-windicss', 'nuxt-schema-org', 'nuxt-icon'],
   content: {
     documentDriven: true,
+    highlight: {
+      theme: 'github-dark',
+    },
   },
   schemaOrg: {
     canonicalHost: 'https://esteban-soubiran.site',
