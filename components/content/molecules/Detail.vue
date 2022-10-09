43d4979cc4f6b4ef715f39e952e75fd1f15eadb3
Barbapapazes <esteban.soubiran@insa-cvl.fr>
2022-10-09 19:19:43 +0200

    fix: detail title color (#315)

--- components/content/molecules/Detail.vue
+++ components/content/molecules/Detail.vue
@@ -6,7 +6,7 @@ defineProps<{
 
 <template>
   <details class="border-1 border-zinc-700/40 bg-zinc-700/10 px-6 py-4 rounded-xl">
-    <summary class="list-none cursor-pointer flex flex-row items-center gap-2 text-zinc-100 text-sm font-semibold">
+    <summary class="list-none cursor-pointer flex flex-row items-center gap-2 text-zinc-300 text-sm font-semibold">
       <Icon name="heroicons:play-20-solid" class="w-3 h-3 transition-transform duration-75 ease-in" data-icon />
       <span>
         {{ title }}
