5204ba759581b325aa7e9ff916e74f24416b9279
Barbapapazes <e.soubiran25@gmail.com>
2022-10-09 16:25:47 +0200

    feat: add page transition

--- app.vue
+++ app.vue
@@ -45,3 +45,16 @@ useSchemaOrg([
     <OrganismsTheFooter class="relative" />
   </div>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.15s ease-in;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(0.5rem);
+}
+</style>
