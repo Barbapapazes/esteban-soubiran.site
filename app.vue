031112ac178a8911f00ba3c0a1fa17e013b45d9f
Barbapapazes <esteban.soubiran@insa-cvl.fr>
2022-10-09 16:17:09 +0200

    fix: add border in bg (#304)

--- app.vue
+++ app.vue
@@ -34,7 +34,7 @@ useSchemaOrg([
 <template>
   <div class="h-full pt-6">
     <div class="fixed inset-0 sm:px-8 lg:px-16">
-      <div class="w-full h-full max-w-screen-xl mx-auto dark:bg-zinc-900"></div>
+      <div class="w-full h-full max-w-screen-xl mx-auto ring-1 dark:bg-zinc-900 dark:ring-zinc-300/20"></div>
     </div>
     <div class="z-90 sticky top-6 flex flex-row justify-center">
       <OrganismsTheHeader />
