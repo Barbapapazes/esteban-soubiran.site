6aac5942cfc0aa41d2d2d74fd7ade3668474346b
Barbapapazes <e.soubiran25@gmail.com>
2022-10-21 15:03:40 +0200

    fix: use tailwind aspect ratio

--- components/content/organisms/HomePictures.vue
+++ components/content/organisms/HomePictures.vue
@@ -16,16 +16,10 @@ defineProps<{
     </h2>
     <div class="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
       <div v-for="image in $props.images" :key="image.src"
-        class="img-container relative aspect-9/10 w-44 flex-none overflow-hidden rounded-xl dark:bg-zinc-800 sm:w-72 sm:rounded-2xl transform"
+        class="img-container relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl dark:bg-zinc-800 sm:w-72 sm:rounded-2xl transform"
         :class="{'rotate-2': image.right, '-rotate-2': image.left}">
         <img :src="image.src" :alt="image.alt" class="absolute h-full w-full object-cover" height="300" loading="lazy">
       </div>
     </div>
   </section>
 </template>
-
-<style scoped>
-.img-container {
-  aspect-ratio: 9 / 10;
-}
-</style>
