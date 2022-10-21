5567c20e0e9ed971a87c351d49b3e9563856f121
Barbapapazes <e.soubiran25@gmail.com>
2022-10-21 16:23:27 +0200

    refactor: home

--- components/content/organisms/ArticlesList.vue
+++ components/content/organisms/ArticlesList.vue
@@ -3,7 +3,12 @@ const { data: articles } = await useAsyncData('articles', () => queryContent('/a
 </script>
 
 <template>
-  <div class="flex flex-col gap-16">
-    <ArticleCard v-for="article in articles" :article="article" :key="article._path" />
-  </div>
+  <section>
+    <h2 class="sr-only">
+      <ContentSlot :use="$slots.default" unwrap="p" />
+    </h2>
+    <div class="flex flex-col gap-16">
+      <ArticleCard v-for="article in articles" :article="article" :key="article._path" />
+    </div>
+  </section>
 </template>
---
+++ components/content/templates/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <AtomsAppSection>
+      <ContentSlot :use="$slots.default" unwrap="p" />
+    </AtomsAppSection>
+    <div class="mt-16 sm:mt-20">
+      <ContentSlot :use="$slots.pictures" unwrap="p" />
+    </div>
+    <AtomsAppSection class="mt-24">
+      <AtomsAppGrid>
+        <ContentSlot :use="$slots.grid" unwrap="p" />
+      </AtomsAppGrid>
+    </AtomsAppSection>
+  </section>
+</template>
