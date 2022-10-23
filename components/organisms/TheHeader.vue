12ac0e755a61e18c8420df81f2fbb5e129400907
Barbapapazes <e.soubiran25@gmail.com>
2022-10-23 10:37:50 +0200

    feat: add mobile button

--- components/organisms/TheHeader.vue
+++ components/organisms/TheHeader.vue
@@ -13,7 +13,7 @@ watch(route, () => {
 <template>
   <header class="sm:px-8 lg:px-16 w-full">
     <div class="max-w-screen-xl mx-auto px-8">
-      <div class="relative max-w-2xl lg:max-w-5xl mx-auto flex justify-center flex-row items-center">
+      <div class="relative max-w-2xl lg:max-w-5xl mx-auto flex justify-end md:justify-center flex-row items-center">
         <Transition>
           <NuxtLink v-if="!isHome" to="/"
             class="absolute left-0 w-10 h-10 flex justify-center items-center ring-1 dark:ring-white/10 rounded-full dark:bg-zinc-800/90">
@@ -21,8 +21,9 @@ watch(route, () => {
               class="block w-9 h-9 rounded-full object-cover backdrop-filter backdrop-blur" loading="async">
           </NuxtLink>
         </Transition>
+
         <div
-          class="rounded-full px-3 shadow-lg shadow-zinc-800/5 ring-1 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10">
+          class="hidden md:flex rounded-full px-3 shadow-lg shadow-zinc-800/5 ring-1 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10">
           <nav aria-labelledby="Navigation primaire">
             <ul class="flex flex-row">
               <li v-for="item in navigation" :key="item._path" class="relative">
@@ -34,8 +35,10 @@ watch(route, () => {
             </ul>
           </nav>
         </div>
+        <MoleculesTheHeaderPopover class="md:hidden"></MoleculesTheHeaderPopover>
       </div>
     </div>
+
   </header>
 </template>
 
