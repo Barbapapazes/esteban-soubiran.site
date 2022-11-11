3ea1b220394ffa479634e60243a57491f687faa7
Barbapapazes <e.soubiran25@gmail.com>
2022-11-11 09:36:41 +0100

    feat: add experience date

--- components/molecules/ExperienceCard.vue
+++ components/molecules/ExperienceCard.vue
@@ -7,30 +7,41 @@ defineProps<{
 </script>
 
 <template>
-  <article class="group relative flex flex-col items-start">
+  <article class="group relative flex flex-col items-start gap-6">
 
     <AtomsExperienceImage :image="experience.company" class="z-20" />
-    <h2 class=" mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
-      <div
-        class="absolute -inset-y-6 -inset-x-4 z-0 scale-95 bg-zinc-50 opacity-0 transition duration-200 ease-in group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl">
-      </div>
-      <NuxtLink :to="experience._path">
-        <span class="absolute -inset-y-6 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
-        <span class="relative z-10">{{
-            experience.title
-        }}</span>
-      </NuxtLink>
-    </h2>
-    <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+
+    <div class="flex flex-col gap-1">
+      <h2 class="text-base font-semibold text-zinc-800 dark:text-zinc-100">
+        <div
+          class="absolute -inset-y-6 -inset-x-4 z-0 scale-95 bg-zinc-50 opacity-0 transition duration-200 ease-in group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl">
+        </div>
+        <NuxtLink :to="experience._path">
+          <span class="absolute -inset-y-6 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
+          <span class="relative z-10">{{
+              experience.title
+          }}</span>
+        </NuxtLink>
+      </h2>
+      <p class="z-10 text-sm font-normal text-zinc-200">{{ experience.status }}</p>
+      <p class="z-10 text-sm font-normal text-zinc-400">{{ experience.from }}
+        <span v-if="experience.to"> -
+          {{ experience.to }}
+        </span>
+      </p>
+    </div>
+
+    <p class="relative z-10text-sm text-zinc-600 dark:text-zinc-400">
       {{ experience.description }}
     </p>
-    <p
-      class="relative z-10 mt-6 flex items-center text-sm font-medium text-zinc-400 transition group-hover:text-sky-500 dark:text-zinc-200">
+
+    <NuxtLink :to="experience.company.url"
+      class="relative z-30 flex items-center text-sm font-medium text-zinc-400 transition hover:text-sky-500 dark:text-zinc-200">
       <Icon name="heroicons:link-20-solid" class="h-4 w-4"></Icon>
       <span class="ml-2">
-        {{ experience.company.url }}
+        {{ experience.company.url.replace('https://', '') }}
       </span>
-    </p>
+    </NuxtLink>
   </article>
 </template>
 
--- content/4.experience/stage.md
+++ content/4.experience/stage.md
@@ -4,8 +4,11 @@ company:
   alt: Photo de profil d'Estéban
   url: https://esteban-soubiran.fr
 position: Développeur web
+status: CDD
 from: 2022
 to: 2022
+datePublished: 2022-01-01
+dateModified: 2022-01-01
 ---
 
 # Un stage
