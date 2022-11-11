7c9fa049b9aabc47178040e45a42c52b0e8e40a0
Barbapapazes <esteban.soubiran@insa-cvl.fr>
2022-11-11 09:23:21 +0100

    feat: add clarity (#353)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      microsoftClarityID: process.env.MICROSOFT_CLARITY_ID,
+    },
+  },
   modules: ['@nuxt/content', '@nuxtjs/tailwindcss', 'nuxt-schema-org', 'nuxt-icon'],
   content: {
     documentDriven: true,
