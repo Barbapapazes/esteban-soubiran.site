02b575916727273a23da1a8e17ca1ad0b91beeec
Barbapapazes <esteban.soubiran@insa-cvl.fr>
2023-04-07 14:26:46 +0200

    chore: update url path to fr (#435)

--- components/organisms/ProjectsList.vue
+++ components/organisms/ProjectsList.vue
@@ -1,16 +1,16 @@
 <script lang="ts" setup>
-import { AsyncData } from "#app"
-import { ParsedContent } from '@nuxt/content/dist/runtime/types';
+import { AsyncData } from '#app'
+import { ParsedContent } from '@nuxt/content/dist/runtime/types'
 
 const props = defineProps<{
   max?: number
-}>();
+}>()
 
 let query: AsyncData<ParsedContent[], true | Error>
 if (props.max) {
-  query = useAsyncData(`projects-${props.max}`, () => queryContent('/projects/').sort({ datePublished: -1 }).limit(props.max).find())
+  query = useAsyncData(`projects-${props.max}`, () => queryContent('/projets/').sort({ datePublished: -1 }).limit(props.max).find())
 } else {
-  query = useAsyncData('projects', () => queryContent('/projects/').sort({ datePublished: -1 }).find())
+  query = useAsyncData('projects', () => queryContent('/projets/').sort({ datePublished: -1 }).find())
 }
 
 const { data: projects } = await query
@@ -19,10 +19,10 @@ const { data: projects } = await query
 <template>
   <section>
     <h2 class="sr-only">
-      <slot></slot>
+      <slot />
     </h2>
     <div class="flex max-w-3xl flex-col space-y-16">
-      <MoleculesProjectCard v-for="project in projects" :project="project" :key="project._path" />
+      <MoleculesProjectCard v-for="project in projects" :key="project._path" :project="project" />
     </div>
   </section>
 </template>
