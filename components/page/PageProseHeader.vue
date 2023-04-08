4e3a8f59b77d047ac6262c162971f24c5a05470b
Barbapapazes <e.soubiran25@gmail.com>
2023-04-08 23:09:19 +0200

    fix: update page header

--- components/page/PageProseHeader.vue
+++ components/page/PageProseHeader.vue
@@ -10,9 +10,9 @@ defineProps<{
 </script>
 
 <template>
-  <div class="max-w-prose mx-auto">
+  <div class="max-w-prose w-full mx-auto">
     <div class="flex flex-col gap-4">
-      <PageTitle class="mt-6">
+      <PageTitle class="mt-4">
         {{ title }}
       </PageTitle>
       <PageDate :datetime="datePublished" class="order-first" />
