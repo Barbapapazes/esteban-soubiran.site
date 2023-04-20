fc2418233826915ab7e7dd4c88973612646a7b24
Barbapapazes <e.soubiran25@gmail.com>
2023-04-20 20:20:20 +0200

    chore(deps): update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  experimental: {
+    watcher: 'parcel'
+  },
+
   extends: ['nuxt-seo-kit'],
   modules: [
     'nuxt-clarity-analytics',
