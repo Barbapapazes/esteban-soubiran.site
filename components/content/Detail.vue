546473b145d6eb07a496fd6de3bcee7b10853e38
Barbapapazes <e.soubiran25@gmail.com>
2023-06-19 09:06:17 +0200

    fix: detail colors in light mode

--- components/content/Detail.vue
+++ components/content/Detail.vue
@@ -5,14 +5,14 @@ defineProps<{
 </script>
 
 <template>
-  <details class="border-1 border-zinc-700/40 bg-zinc-700/10 px-6 py-4 rounded-xl">
-    <summary class="list-none cursor-pointer flex flex-row items-center gap-2 text-zinc-300 text-sm font-semibold">
+  <details class="border-1 border-zinc-300/40 dark:border-zinc-700/40 bg-zinc-300/10 dark:bg-zinc-700/10 px-6 py-4 rounded-xl">
+    <summary class="list-none cursor-pointer flex flex-row items-center gap-2 text-zinc-500 dark:text-zinc-300 text-sm font-semibold">
       <Icon name="heroicons:play-20-solid" class="w-3 h-3 transition-transform duration-75 ease-in" data-icon />
       <span>
         {{ title }}
       </span>
     </summary>
-    <div data-content class="mt-2 text-zinc-300 text-sm">
+    <div data-content class="mt-2 text-zinc-500 dark:text-zinc-300 text-sm">
       <ContentSlot :use="$slots.default" />
     </div>
   </details>
