b718b9828e471c465a3723e66a61ef7e97a63f95
Barbapapazes <e.soubiran25@gmail.com>
2023-07-05 00:35:29 +0200

    fix: margin around prose-code

--- components/content/ProseCode.vue
+++ components/content/ProseCode.vue
@@ -10,7 +10,7 @@ const { copy, copied, isSupported } = useClipboard()
 </script>
 
 <template>
-  <div class="bg-white dark:bg-zinc-900 rounded-xl border-1 border-zinc-300/40 dark:border-zinc-700/40 overflow-hidden">
+  <div class="my-4 bg-white dark:bg-zinc-900 rounded-xl border-1 border-zinc-300/40 dark:border-zinc-700/40 overflow-hidden">
     <div class="group relative bg-zinc-300/10 dark:bg-zinc-700/10 px-4 py-4">
       <span v-if="filename" class="absolute top-2 right-3 text-xs font-mono">
         {{ filename }}
