b540184ed05e3aa6a0500d864ebdde804e18fb05
barbapapazes <e.soubiran25@gmail.com>
2023-10-21 18:00:40 +0200

    fix: prose nav

--- components/prose/nav/ProseNavToc.vue
+++ components/prose/nav/ProseNavToc.vue
@@ -16,15 +16,15 @@ function isActive(id: string) {
 <template>
   <ul class="text-zinc-500 dark:text-zinc-400 text-sm space-y-2">
     <li v-for="link in toc.links" :key="link.id">
-      <ArticlesNavGroupItem :is="NuxtLink" :to="`#${link.id}`" class="block" :class="isActive(link.id) ? 'text-primary!' : ''">
+      <ProseNavGroupItem :is="NuxtLink" :to="`#${link.id}`" class="block" :class="isActive(link.id) ? 'text-primary!' : ''">
         {{ link.text }}
-      </ArticlesNavGroupItem>
+      </ProseNavGroupItem>
       <template v-if="link.children">
         <ul class="ml-2 pl-4 my-2 border-l-1 border-zinc-300 dark:border-zinc-700 space-y-2">
           <li v-for="child in link.children" :key="child.id">
-            <ArticlesNavGroupItem :is="NuxtLink" :to="`#${child.id}`" class="block" :class="isActive(child.id) ? 'text-primary!' : ''">
+            <ProseNavGroupItem :is="NuxtLink" :to="`#${child.id}`" class="block" :class="isActive(child.id) ? 'text-primary!' : ''">
               {{ child.text }}
-            </ArticlesNavGroupItem>
+            </ProseNavGroupItem>
           </li>
         </ul>
       </template>
