cc2f0ae7c786f05fab61634c2ede3dcd2b23c7d3
barbapapazes <e.soubiran25@gmail.com>
2023-10-21 16:56:40 +0200

    fix: search

--- composables/useSearch.ts
+++ composables/useSearch.ts
@@ -4,7 +4,7 @@ export async function useSearch(search: MaybeRefOrGetter<string>): Promise<Compu
   const { data: searchData } = await useFetch<string>('/api/search', { lazy: true, server: false })
 
   const data = computed(() => {
-    return searchData.value ?? '[]'
+    return searchData.value ?? JSON.stringify({ serializationVersion: 2, documentIds: {}, documentCount: 0, index: [], nextId: 0, fieldIds: {}, fieldLength: {}, averageFieldLength: [], storedFields: {}, dirtCount: 0 })
   })
 
   const { results } = useIndexedMiniSearch(search, data, {
